f47038f3b4aa2834f9950ede3a774287d8d66c7f
Jim Gray <jgray8689@gmail.com>
2022-02-05 08:06:46 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
