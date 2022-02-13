09c0f10edee956070ddf978bb0063fd39f63e672
Jim Gray <jgray8689@gmail.com>
2022-02-13 16:43:33 -0500

    setup images and css classNames

--- src/components/Card.js
+++ src/components/Card.js
@@ -25,15 +25,17 @@ Notes:
 export default function Card() {
   return (
     <div className="card">
-      <img src={katieZaferes} alt="Katie Zaferes" />
-      <img src={star} alt="star" />
-      <h5>5.0 (6)</h5>
-      <img src={ellipse} alt="ellipse" />
-      <h5>USA</h5>
-      <h4>Life lessons with Katie Zaferes</h4>
-      <h4>
-        <b>From $136</b> / person
-      </h4>
+      <img src={katieZaferes} alt="Katie Zaferes" className="card--image" />
+      <div className="cardStats">
+        <img src={star} alt="star" className="card--star" />
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray"> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136</span> / person
+      </p>
     </div>
   );
 }
